c1ca1e09d02def3073f7b71c129270ae66800057
oyinlomo <saheedbarakat24@gmail.com>
2024-09-11 14:29:00 +0100

    changedd

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import React,{useEffect} from "react";
 import AOS from'aos'
 import 'aos/dist/aos.css';
+import Body from "./Body";
 
 
 
@@ -13,12 +14,16 @@ const Header = (props) => {
     });
   }, []);
   
+ function mode() {
+  document.body.classList.toggle('darktheme')
+  
+ }
   return (
     <>
       <div className="nav" data-aos='fade-up'>
         <h3 className="logo">
         {props.myName}
-        </h3>
+        </h3> 
         <ul className="nav-list" id="nav">
           <a href="#work"><li>Work</li></a>
           <a href="#about">
@@ -29,7 +34,10 @@ const Header = (props) => {
           <a href="#contact"><li>Contact</li></a>
           
         </ul>
+        <button onClick={mode} id="mode" className="mode">
+        <i class="ri-moon-fill"></i>
         
+        </button>
       </div>
     <div>
    
